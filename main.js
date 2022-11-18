c49a100e1b7adbaf5a75f1ce07d5ac8c6337f80d
sohamchakraborty09 <86429132+sohamchakraborty09@users.noreply.github.com>
2022-11-18 17:33:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+var status = "";
+var objects = [];
+var percent = "";
+var video = "";
+
+function preload(){
+    song = loadSound("buzzer.mp3");  
+}
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+function modelLoaded(){
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(video, gotResult)
+}
+function gotResult(error, results) {
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+function draw(){
+    image(video, 0, 0, 380, 380);
+
+    if(status !=""){
+        var r = random(255);
+        var g = random(255);
+        var b = random(255);
+        objectDetector.detect(video, gotResult); 
+       for(i=0; i < objects.length; i++){
+          document.getElementById("status").innerHTML = "Status : Object Detected";
+          document.getElementById("number_of_objects").innerHTML = "number of object(s) detected is:" + objects.length;
+
+          fill(r, g, b);
+          percent =  floor(objects[i].confidence*100);
+          text(objects[i].label+ "" + percent + "%", objects[i].x, objects[i].y);
+          noFill();
+          stroke(r, g, b);
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+         
+       }
+    }
+}
+              
